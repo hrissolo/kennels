cb13f2d0a17b0dbb0d78f7c7fa7c9a7717f4ddc7
hrissolo <hrissolo@gmail.com>
2020-10-02 13:17:26 -0500

    added components for all

---
+++ src/components/location/LocationCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = () => (
+    <section className="location">
+        <h3 className="location__name">East Side</h3>
+        <div className="location__address">3000 East Way</div>
+    </section>
+)
